9ff95486e69603b449ce6d6636ca24311a17f6ae
Hemario <kevin.de.smet@outlook.be>
2018-08-24 20:44:00 +0200

    Added icyveins_paladin_protection

--- Ovale_TankScripts.lua
+++ Ovale_TankScripts.lua
@@ -7,6 +7,7 @@ OvaleScripts.GetDefaultScriptName = function(self, className, specialization)
     local scriptName
     if (className == "DRUID" and specialization == "guardian")
         or (className == "MONK" and specialization == "brewmaster") 
+        or (className == "PALADIN" and specialization == "protection")
         or (className == "WARRIOR" and specialization == "protection")
     then
         scriptName = format("icyveins_%s_%s", lower(className), specialization)
