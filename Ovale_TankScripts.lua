c1bcf61ff9b226697f55b4cd18e0424b4c8b7f1a
Hemario <kevin.de.smet@outlook.be>
2018-08-24 20:52:02 +0200

    Added icyveins_demonhunter_vengeance

--- Ovale_TankScripts.lua
+++ Ovale_TankScripts.lua
@@ -5,7 +5,9 @@ local OvaleScripts = __Scripts.OvaleScripts
 local baseGetDefaultScriptName = OvaleScripts.GetDefaultScriptName
 OvaleScripts.GetDefaultScriptName = function(self, className, specialization)
     local scriptName
-    if (className == "DRUID" and specialization == "guardian")
+    if false
+        or (className == "DEMONHUNTER" and specialization == "vengeance") 
+        or (className == "DRUID" and specialization == "guardian")
         or (className == "MONK" and specialization == "brewmaster") 
         or (className == "PALADIN" and specialization == "protection")
         or (className == "WARRIOR" and specialization == "protection")
