528518c7484b0642964d023fbe3ede98bda8e342
Hemario <kevin.de.smet@outlook.be>
2018-09-16 19:30:04 +0200

    spaces to tab

--- scripts/monk.lua
+++ scripts/monk.lua
@@ -1,9 +1,9 @@
 local __Scripts = LibStub:GetLibrary("ovale/Scripts")
 local OvaleScripts = __Scripts.OvaleScripts
 do
-    local name = "icyveins_monk_brewmaster"
-    local desc = "[8.0.1] Icy-Veins: Monk Brewmaster"
-    local code = [[
+	local name = "icyveins_monk_brewmaster"
+	local desc = "[8.0.1] Icy-Veins: Monk Brewmaster"
+	local code = [[
 Include(ovale_common)
 Include(ovale_trinkets_mop)
 Include(ovale_trinkets_wod)
@@ -51,18 +51,18 @@ AddFunction BrewmasterRangeCheck
 AddFunction BrewmasterDefaultShortCDActions
 {
 	# keep ISB up always when taking dmg
-    if ((BaseDuration(light_stagger_debuff)-DebuffRemaining(any_stagger_debuff)<5 or target.IsTargetingPlayer()) and BuffExpires(ironskin_brew_buff 3) and BuffExpires(blackout_combo_buff)) Spell(ironskin_brew text=min)
+	if ((BaseDuration(light_stagger_debuff)-DebuffRemaining(any_stagger_debuff)<5 or target.IsTargetingPlayer()) and BuffExpires(ironskin_brew_buff 3) and BuffExpires(blackout_combo_buff)) Spell(ironskin_brew text=min)
 	
 	# keep stagger below 100% (70% when in a party, 30% when BOB is up)
 	if (StaggerPercentage() >= 100 or (StaggerPercentage() >= 70 and not UnitInRaid()) or (StaggerPercentage() >= 30 and Talent(black_ox_brew_talent) and SpellCooldown(black_ox_brew) <= 0)) Spell(purifying_brew)
 	# use black_ox_brew when at 0 charges and low energy (or in an emergency)
-    if (SpellCharges(ironskin_brew count=0) <= 0.75)
-    {
-        #black_ox_brew,if=incoming_damage_1500ms&stagger.heavy&cooldown.brews.charges_fractional<=0.75
-        if IncomingDamage(1.5) > 0 and DebuffPresent(heavy_stagger_debuff) Spell(black_ox_brew)
-        #black_ox_brew,if=(energy+(energy.regen*cooldown.keg_smash.remains))<40&buff.blackout_combo.down&cooldown.keg_smash.up
-        if Energy() + EnergyRegenRate() * SpellCooldown(keg_smash) < 40 and BuffExpires(blackout_combo_buff) and not SpellCooldown(keg_smash) > 0 Spell(black_ox_brew)
-    }
+	if (SpellCharges(ironskin_brew count=0) <= 0.75)
+	{
+		#black_ox_brew,if=incoming_damage_1500ms&stagger.heavy&cooldown.brews.charges_fractional<=0.75
+		if IncomingDamage(1.5) > 0 and DebuffPresent(heavy_stagger_debuff) Spell(black_ox_brew)
+		#black_ox_brew,if=(energy+(energy.regen*cooldown.keg_smash.remains))<40&buff.blackout_combo.down&cooldown.keg_smash.up
+		if Energy() + EnergyRegenRate() * SpellCooldown(keg_smash) < 40 and BuffExpires(blackout_combo_buff) and not SpellCooldown(keg_smash) > 0 Spell(black_ox_brew)
+	}
 	
 	# heal me
 	BrewmasterHealMeShortCd()
@@ -77,10 +77,10 @@ AddFunction BrewmasterDefaultShortCDActions
 		# always bank 1 charge
 		unless (SpellCharges(ironskin_brew) <= 1)
 		{
-            # keep ISB rolling
-            if BuffRemaining(ironskin_brew_buff) < DebuffRemaining(any_stagger_debuff) and BuffExpires(blackout_combo_buff) Spell(ironskin_brew)
+			# keep ISB rolling
+			if BuffRemaining(ironskin_brew_buff) < DebuffRemaining(any_stagger_debuff) and BuffExpires(blackout_combo_buff) Spell(ironskin_brew)
 			
-            # never be at (almost) max charges 
+			# never be at (almost) max charges 
 			unless (SpellFullRecharge(ironskin_brew) > 3)
 			{
 				if (BuffRemaining(ironskin_brew_buff) < 2*BaseDuration(ironskin_brew_buff) and BuffExpires(blackout_combo_buff)) Spell(ironskin_brew text=max)
@@ -96,9 +96,9 @@ AddFunction BrewmasterDefaultShortCDActions
 
 AddFunction BrewmasterDefaultMainActions
 {
-    BrewmasterHealMeMain()
-    if (not InCombat()) Spell(keg_smash)
-        
+	BrewmasterHealMeMain()
+	if (not InCombat()) Spell(keg_smash)
+		
 	if Talent(blackout_combo_talent) BrewmasterBlackoutComboMainActions()
 	unless Talent(blackout_combo_talent) 
 	{
@@ -135,16 +135,16 @@ AddFunction BrewmasterBlackoutComboMainActions
 
 AddFunction BrewmasterDefaultAoEActions
 {
-    BrewmasterHealMeMain()
-    if (not InCombat()) Spell(keg_smash)
+	BrewmasterHealMeMain()
+	if (not InCombat()) Spell(keg_smash)
  
-    if (Talent(blackout_combo_talent) and not BuffPresent(blackout_combo_buff)) Spell(blackout_strike)
+	if (Talent(blackout_combo_talent) and not BuffPresent(blackout_combo_buff)) Spell(blackout_strike)
 	if (not Talent(blackout_combo_talent) or (BuffPresent(blackout_combo_buff) and SpellCharges(ironskin_brew) <= SpellData(ironskin_brew charges)-2) or SpellFullRecharge(keg_smash) == 0) Spell(keg_smash)
 	Spell(chi_burst)
 	Spell(chi_wave)
 	if (target.DebuffPresent(keg_smash) and not BuffPresent(blackout_combo_buff)) Spell(breath_of_fire)
 	if (BuffRefreshable(rushing_jade_wind_buff)) Spell(rushing_jade_wind)
-    Spell(arcane_pulse)
+	Spell(arcane_pulse)
 	if (Energy() >= 65 or (Talent(black_ox_brew_talent) and SpellCooldown(black_ox_brew) <= 0)) Spell(tiger_palm)
 	if (not BuffPresent(blackout_combo_buff)) Spell(blackout_strike)	
 }
@@ -159,11 +159,11 @@ AddFunction BrewmasterDefaultCdActions
 	Spell(fortifying_brew)
 	Spell(dampen_harm)
 	if CheckBoxOn(opt_use_consumables) 
-    {
-        Item(battle_potion_of_agility usable=1)
-        Item(steelskin_potion usable=1)
-        Item(battle_potion_of_stamina usable=1)
-    }
+	{
+		Item(battle_potion_of_agility usable=1)
+		Item(steelskin_potion usable=1)
+		Item(battle_potion_of_stamina usable=1)
+	}
 	Spell(zen_meditation)
 	UseRacialSurvivalActions()
 }
@@ -200,5 +200,5 @@ AddIcon help=cd specialization=brewmaster
 	BrewmasterDefaultCdActions()
 }
 ]]
-    OvaleScripts:RegisterScript("MONK", "brewmaster", name, desc, code, "script")
+	OvaleScripts:RegisterScript("MONK", "brewmaster", name, desc, code, "script")
 end
--- scripts/warrior.lua
+++ scripts/warrior.lua
@@ -1,9 +1,9 @@
 local __Scripts = LibStub:GetLibrary("ovale/Scripts")
 local OvaleScripts = __Scripts.OvaleScripts
 do
-    local name = "icyveins_warrior_protection"
-    local desc = "[8.0.1] Icy-Veins: Warrior Protection"
-    local code = [[
+	local name = "icyveins_warrior_protection"
+	local desc = "[8.0.1] Icy-Veins: Warrior Protection"
+	local code = [[
 Include(ovale_common)
 Include(ovale_trinkets_mop)
 Include(ovale_trinkets_wod)
@@ -39,14 +39,14 @@ AddFunction ProtectionInterruptActions
 	{
 		if target.InRange(pummel) and target.IsInterruptible() Spell(pummel)
 		if not target.Classification(worldboss) 
-        {
-            if target.InRange(storm_bolt) Spell(storm_bolt)
-            if target.InRange(intercept) and Talent(warbringer_talent) Spell(intercept)
-            if target.Distance(less 10) Spell(shockwave)
-            if target.InRange(quaking_palm) Spell(quaking_palm)
-            if target.Distance(less 5) Spell(war_stomp)
-            if target.InRange(intimidating_shout) Spell(intimidating_shout)
-        }
+		{
+			if target.InRange(storm_bolt) Spell(storm_bolt)
+			if target.InRange(intercept) and Talent(warbringer_talent) Spell(intercept)
+			if target.Distance(less 10) Spell(shockwave)
+			if target.InRange(quaking_palm) Spell(quaking_palm)
+			if target.Distance(less 5) Spell(war_stomp)
+			if target.InRange(intimidating_shout) Spell(intimidating_shout)
+		}
 	}
 }
 
@@ -63,14 +63,14 @@ AddFunction ProtectionOffensiveCooldowns
 AddFunction ProtectionDefaultShortCDActions
 {
 	ProtectionHealMe()
-    if (BuffRemaining(shield_block_buff) < 2*BaseDuration(shield_block_buff)) 
-    {
-        if IncomingDamage(5 physical=1)
-            and (not Talent(bolster_talent) or not BuffPresent(last_stand_buff)) 
-        {
-            Spell(shield_block)
-        }
-    }
+	if (BuffRemaining(shield_block_buff) < 2*BaseDuration(shield_block_buff)) 
+	{
+		if IncomingDamage(5 physical=1)
+			and (not Talent(bolster_talent) or not BuffPresent(last_stand_buff)) 
+		{
+			Spell(shield_block)
+		}
+	}
 	if (IncomingDamage(5) and Rage() >= 70) Spell(ignore_pain)
 	# range check
 	ProtectionGetInMeleeRange()
@@ -82,14 +82,14 @@ AddFunction ProtectionDefaultShortCDActions
 
 AddFunction ProtectionDefaultMainActions
 {
-    if not BuffPresent(battle_shout_buff) Spell(battle_shout)
+	if not BuffPresent(battle_shout_buff) Spell(battle_shout)
 	Spell(shield_slam)
-    Spell(dragon_roar)
+	Spell(dragon_roar)
 	Spell(thunder_clap)
 	if BuffPresent(revenge_buff) Spell(revenge)
 	if not target.Classification(worldboss) Spell(storm_bolt)
-    if RageDeficit() <= 20 Spell(revenge)
-    Spell(devastate)
+	if RageDeficit() <= 20 Spell(revenge)
+	Spell(devastate)
 }
 
 #
@@ -99,12 +99,12 @@ AddFunction ProtectionDefaultMainActions
 AddFunction ProtectionDefaultAoEActions
 {
 	if not BuffPresent(battle_shout_buff) Spell(battle_shout)
-    Spell(ravager)
-    Spell(dragon_roar)
-    if (Talent(best_served_cold_talent) and Enemies()>= 2) Spell(revenge)
-    Spell(thunder_clap)
-    Spell(revenge)
-    if not BuffPresent(shield_block_buff) and Enemies() >= 2+Talent(rumbling_earth_talent) Spell(shockwave)
+	Spell(ravager)
+	Spell(dragon_roar)
+	if (Talent(best_served_cold_talent) and Enemies()>= 2) Spell(revenge)
+	Spell(thunder_clap)
+	Spell(revenge)
+	if not BuffPresent(shield_block_buff) and Enemies() >= 2+Talent(rumbling_earth_talent) Spell(shockwave)
 	Spell(shield_slam)
 	if Enemies() >= 3 Spell(shockwave)
 	Spell(devastate)
@@ -123,14 +123,14 @@ AddFunction ProtectionDefaultCdActions
 	Item(Trinket1Slot usable=1 text=14)
 	Spell(demoralizing_shout)
 	if not Talent(bolster_talent) or not BuffPresent(shield_block_buff) Spell(last_stand)
-    Spell(shield_wall)
+	Spell(shield_wall)
 	if CheckBoxOn(opt_use_consumables) 
-    {
-        Item(battle_potion_of_agility usable=1)
-        Item(steelskin_potion usable=1)
-        Item(battle_potion_of_stamina usable=1)
-    }
-    if not BuffPresent(rallying_cry_buff) Spell(rallying_cry)
+	{
+		Item(battle_potion_of_agility usable=1)
+		Item(steelskin_potion usable=1)
+		Item(battle_potion_of_stamina usable=1)
+	}
+	if not BuffPresent(rallying_cry_buff) Spell(rallying_cry)
 }
 
 #
@@ -157,5 +157,5 @@ AddIcon help=cd specialization=protection
 	ProtectionDefaultCdActions()
 }
 ]]
-    OvaleScripts:RegisterScript("WARRIOR", "protection", name, desc, code, "script")
+	OvaleScripts:RegisterScript("WARRIOR", "protection", name, desc, code, "script")
 end
