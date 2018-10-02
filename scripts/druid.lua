b535f5b2edbc8574af491c5eb5c5e946e03bed5a
Hemario <kevin.de.smet@outlook.be>
2018-10-02 05:02:25 +0200

    DRUID: get 3 stacks of thrash when incarnation

--- scripts/druid.lua
+++ scripts/druid.lua
@@ -84,6 +84,7 @@ AddFunction GuardianDefaultAoEActions
 	{
 		if (DebuffCountOnAny(moonfire_debuff) < 1) Spell(moonfire)
 		if (BuffRefreshable(pulverize_buff)) Spell(pulverize)
+        if (target.DebuffStacks(thrash_bear_debuff) < 3) Spell(thrash_bear)
 		if (Enemies() <= 3) Spell(mangle)
 		Spell(thrash_bear)
 	}
