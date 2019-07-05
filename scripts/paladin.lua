326d83deb845e1a01caafc7547d7948cdd65f678
Hemario <kevin.de.smet@outlook.be>
2019-07-06 01:14:46 +0200

    PALADIN: can use bubble when solo

--- scripts/paladin.lua
+++ scripts/paladin.lua
@@ -102,8 +102,8 @@ AddFunction ProtectionDefaultCdActions
         Spell(ardent_defender)
         Spell(guardian_of_ancient_kings)
         Spell(aegis_of_light)
-        if Talent(final_stand_talent) Spell(divine_shield)
-        if CheckBoxOn(opt_use_consumables) 
+        if (Talent(final_stand_talent) or not UnitInParty()) Spell(divine_shield)
+        if (CheckBoxOn(opt_use_consumables)) 
         {
             Item(item_steelskin_potion usable=1)
             Item(item_battle_potion_of_stamina usable=1)
