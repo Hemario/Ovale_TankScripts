b60c85b65cd72a0e301501e19be922b732e6983f
Hemario <kevin.de.smet@outlook.be>
2019-07-15 18:17:39 +0200

    just a ()

--- scripts/common.lua
+++ scripts/common.lua
@@ -18,7 +18,7 @@ AddFunction AzeriteEssenceOffensiveCooldowns
 
 AddFunction AzeriteEssenceDefensiveCooldowns
 {
-    if not UnitClass(paladin) Spell(memory_of_lucid_dreams_essence)
+    if (not UnitClass(paladin)) Spell(memory_of_lucid_dreams_essence)
 }
 
 ]]
