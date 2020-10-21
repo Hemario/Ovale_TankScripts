666e71fea9c29dc9a82874baa37b9623b5bb0248
Hemario <kevin.de.smet@outlook.be>
2020-10-21 20:15:24 +0200

    only enable monk and paladin for now

--- Ovale_TankScripts.lua
+++ Ovale_TankScripts.lua
@@ -6,12 +6,12 @@ local baseGetDefaultScriptName = OvaleScripts.GetDefaultScriptName
 OvaleScripts.GetDefaultScriptName = function(self, className, specialization)
     --print("Ovale_TankScripts GetDefaultScriptName")
     if false
-        or (className == "DEATHKNIGHT" and specialization == "blood") 
-        or (className == "DEMONHUNTER" and specialization == "vengeance") 
-        or (className == "DRUID" and specialization == "guardian")
+        --or (className == "DEATHKNIGHT" and specialization == "blood") 
+        --or (className == "DEMONHUNTER" and specialization == "vengeance") 
+        --or (className == "DRUID" and specialization == "guardian")
         or (className == "MONK" and specialization == "brewmaster") 
         or (className == "PALADIN" and specialization == "protection")
-        or (className == "WARRIOR" and specialization == "protection")
+        --or (className == "WARRIOR" and specialization == "protection")
     then
         return format("ovale_tankscripts_%s_%s", string.lower(className), specialization)
     else
