0c86105fe7e2b8577de82c539d790ac2f32bba33
Hemario <kevin.de.smet@outlook.be>
2021-01-22 18:50:43 +0100

    moved trinket definitions to Ovale

--- scripts/common.lua
+++ scripts/common.lua
@@ -6,19 +6,6 @@ do
     local code = [[
 Include(ovale_common)
 
-Define(bottled_flayedwing_toxin 178742)
-    ItemRequire(bottled_flayedwing_toxin unusable set=1 enabled=(player.BuffPresent(bottled_flayedwing_toxin_buff)))
-Define(bottled_flayedwing_toxin_buff 345545)
-Define(grim_codex 178811)
-    ItemRequire(grim_codex unusable set=1 enabled=(target.isfriend()))
-Define(mistcaller_ocarina 178715)
-    ItemRequire(mistcaller_ocarina unusable set=1 enabled=(InCombat() or player.BuffPresent(mistcaller_ocarina_buff)))
-Define(mistcaller_ocarina_crit_buff 332299)
-Define(mistcaller_ocarina_haste_buff 332300)
-Define(mistcaller_ocarina_mastery_buff 332301)
-Define(mistcaller_ocarina_vers_buff 330067)
-SpellList(mistcaller_ocarina_buff mistcaller_ocarina_crit_buff mistcaller_ocarina_haste_buff mistcaller_ocarina_mastery_buff mistcaller_ocarina_vers_buff)
-
 Define(item_abyssal_healing_potion 169451)
 Define(item_coastal_healing_potion 152494)
 Define(item_healthstone 5512)
